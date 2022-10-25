cf32149c61e84c47a11cd1bd5828cc58499a303b
Felipe Randon <feliperandon32@gmail.com>
2022-10-25 14:43:48 -0300

    Special Move - Castling

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -34,8 +34,8 @@ public class Bishop extends ChessPiece {
 		// ne
 		p.setValues(position.getRow() - 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-		mat[p.getRow()][p.getColumn()] = true;
-		p.setValues(p.getRow() - 1, p.getColumn() + 1);
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 		mat[p.getRow()][p.getColumn()] = true;
@@ -44,8 +44,8 @@ public class Bishop extends ChessPiece {
 		// se 
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-		mat[p.getRow()][p.getColumn()] = true;
-		p.setValues(p.getRow() + 1, p.getColumn() + 1);
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 		mat[p.getRow()][p.getColumn()] = true;
